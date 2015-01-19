4568c7ea72d84253336501be6dd026c3f8fa3e5c
Leandro Menezes dos Anjos <grilo106@gmail.com>
2015-01-19 10:52:33 -0200

    Update RegisterDatabaseListener.php
    Tratando caso a chave databases não exista no config

--- Bootstrap/RegisterDatabaseListener.php
+++ Bootstrap/RegisterDatabaseListener.php
@@ -8,32 +8,33 @@ class RegisterDatabaseListener
     {
         $di = $application->getDI();
         $config = $di->get('config');
-
-        foreach ($config['databases'] as $k => $database) {
-
-            $db_name = 'db_'.$k;
-
-            $di->set($db_name, function() use ($database){
-
-                $config = [
-                    'host' => $database->host,
-                    'dbname' => $database->dbname,
-                    'username' => $database->username,
-                    'password' => $database->password,
-                ];
-
-                if (isset($database->port)) {
-
-                    $config['port'] = (int)$database->port;
-
-                }
-
-                $connection = new \Phalcon\Db\Adapter\Pdo\Mysql($config);
-
-                return $connection;
-
-            });
-
+        if(isset($config['databases']) && is_array($config['databases'])){
+            foreach ($config['databases'] as $k => $database) {
+    
+                $db_name = 'db_'.$k;
+    
+                $di->set($db_name, function() use ($database){
+    
+                    $config = [
+                        'host' => $database->host,
+                        'dbname' => $database->dbname,
+                        'username' => $database->username,
+                        'password' => $database->password,
+                    ];
+    
+                    if (isset($database->port)) {
+    
+                        $config['port'] = (int)$database->port;
+    
+                    }
+    
+                    $connection = new \Phalcon\Db\Adapter\Pdo\Mysql($config);
+    
+                    return $connection;
+    
+                });
+    
+            }
         }
 
     }
